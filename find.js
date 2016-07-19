369b89dcd4cca9e5b63e0c20b3fdd46e54e239a8
ivan alejandro <ivan999art@gmail.com>
2016-07-19 09:23:55 -0500

    A01 S3  learn bueno

--- find.js
+++ find.js
@@ -1,12 +1,12 @@
 // 1. Rescatando el argumento que es pasado  al script 
-var ageArg = +process.argv[2];
+var ageArg = Number(process.argv[2]);
 // Conectamos a la base de datos 
 // Pasp 1. Carga el driver en nuestro script
 var mongodb = require('mongodb');
 
 //Paso 2. El Driver de MongoDB nos proporciona un cliente 
 //Por lo que lo estraemos de la libreia 
-var mongoClient = mongodb.mongoClient;
+var mongoClient = mongodb.MongoClient;
 //Paso 3. Conectamos el cliente con la base de datos 
  mongoClient.connect("mongodb://127.0.0.1:27017/learnyoumongo",
  function (err,db ) {
